ef41bda01cc7ca4d067677c38e089c64354805ef
Bui Duc Trung <buiductrung.cva@gmail.com>
2025-11-23 17:06:07 +0700

    Lesson 5

---
+++ Lesson5/main.py
@@ -0,0 +1,11 @@
+# Đề bài: Quản lý môn học của học sinh
+# Yêu cầu:
+    # Mỗi sinh viên có tên, nhiều môn học
+    # Mỗi môn học có tên môn và điểm số
+    # Tạo menu:
+        # Add a new student
+        # Add subject for a student
+        # Calculate average score of a student
+        # Calculate average score of all students
+        # Save data to file
+        # Exit
