f41f53874d4b230a978d64370fdc05f7c7534aa1
Bui Duc Trung <buiductrung.cva@gmail.com>
2026-01-18 17:48:18 +0700

    Lesson 13

---
+++ Lesson13/practice.py
@@ -0,0 +1,6 @@
+# Bài 1: Sử dụng OOP để mô phỏng bộ bài tú lơ khơ (52 lá)
+# Yêu cầu:
+    # - 2 class: Card (lá bài) và Deck (bộ bài)
+    # - Class Card có các thuộc tính: rank (hạng bài), suit (chất bài)
+    # - Class Deck có thuộc tính: cards (danh sách các lá bài)
+    # - Class Deck có các phương thức: shuffle (xáo bài), deal (chia bài), display (hiển thị)
